9a58edc3cd559a057e06ba176603616de72b8834
Mostafa Ghanbari <mostafa.qanbary@gmail.com>
2020-10-15 23:01:55 -0400

    Finish setting up AWS Amplify

--- src/main.js
+++ src/main.js
@@ -3,5 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/tailwind.css'
+import Amplify from 'aws-amplify'
+import '@aws-amplify/ui-vue'
+import aws_exports from './aws-exports'
+
+Amplify.configure(aws_exports)
 
 createApp(App).use(store).use(router).mount('#app')
