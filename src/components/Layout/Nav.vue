6e2614fba4a5a8b7c09cdf2a9b36e508da3715b6
Mostafa Ghanbari <mostafa.qanbary@gmail.com>
2020-10-16 20:33:28 -0400

    Navigation component functionality using mapGetters vuex helper

---
+++ src/components/Layout/Nav.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  export default {
+    name: 'Navbar',
+    methods: {
+      async logout() {
+        await this.$store.dispatch('auth/logout')
+        this.$router.push('/')
+      }
+    },
+    computed: {
+      ...mapGetters({
+        user: 'auth/user'
+      })
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
